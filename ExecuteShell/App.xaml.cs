42b4a2f63ddfbad9b15aea991e663092816bfe23
HuaMouRen <2825338723@qq.com>
2026-03-13 20:44:09 +0800

    feat: 初始化项目 `ExecuteShell`

---
+++ ExecuteShell/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using ExecuteShell.Windows;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ExecuteShell;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    private void Application_Startup(object sender, StartupEventArgs e)
+    {
+        var window = new WindowMain();
+        this.MainWindow = window;
+        window.Show();
+    }
+}
+
