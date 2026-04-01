5fb19c300a78bbe73a7b6494074382dcf7116924
HuaMouRen <2825338723@qq.com>
2026-04-01 18:56:46 +0800

    feat(window-overlay): 完成基本覆盖层切换功能

--- PvzLauncherRemake/Windows/WindowOverlay.xaml.cs
+++ PvzLauncherRemake/Windows/WindowOverlay.xaml.cs
@@ -1,5 +1,8 @@
-﻿using PvzLauncherRemake.Classes;
+﻿using NHotkey;
+using NHotkey.Wpf;
+using PvzLauncherRemake.Classes;
 using PvzLauncherRemake.Utils.Services;
+using PvzLauncherRemake.Utils.UI;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -21,6 +24,7 @@ namespace PvzLauncherRemake.Windows
     public partial class WindowOverlay : Window
     {
         private DispatcherTimer? _timer;
+        private bool IsOverlayVisible = true;
 
         public WindowOverlay()
         {
@@ -30,19 +34,13 @@ namespace PvzLauncherRemake.Windows
                 Interval = TimeSpan.FromMilliseconds(10)
             };
             _timer.Tick += Timer_Tick;
-
-
-            Loaded += ((s, e) => _timer.Start());
         }
 
         private void Timer_Tick(object? sender, EventArgs e)
         {
             //如游戏退出或进程信息为空则关闭覆盖界面
-            if (!GameManager.IsGameRuning || AppProcess.Process == null || AppProcess.Process.HasExited) 
-            {
-                _timer?.Stop();_timer = null;
+            if (!GameManager.IsGameRuning || AppProcess.Process == null || AppProcess.Process.HasExited)
                 this.Close();
-            }
 
             AppProcess.Process!.Refresh();
 
@@ -54,6 +52,49 @@ namespace PvzLauncherRemake.Windows
             this.Width = result.Width;
             this.Height = result.Height;
 
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _timer?.Start();
+            ToggleOverlay(false);
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            _timer?.Stop();
+            _timer = null;
+        }
+
+        private void Window_SourceInitialized(object sender, EventArgs e)
+        {
+            //注册热键
+            try
+            {
+                //切换覆盖层显示
+                HotkeyManager.Current.AddOrReplace("ToggleOverlay", Key.P, ModifierKeys.Control | ModifierKeys.Alt, ((s, e) => ToggleOverlay()));
+            }
+            catch (Exception ex)
+            {
+                ErrorReportDialog.Show(ex);
+            }
+        }
+
+        private void ToggleOverlay(bool? targetState = null)
+        {
+            if (targetState == null)
+                IsOverlayVisible = !IsOverlayVisible;
+            else
+                IsOverlayVisible = (bool)targetState;
+
+            if (IsOverlayVisible)
+            {
+                this.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                this.Visibility = Visibility.Hidden;
+            }
         }
     }
 }
